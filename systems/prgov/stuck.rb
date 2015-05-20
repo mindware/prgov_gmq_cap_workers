96757cc1937c6f93538eaad4a05364062a9cf52b
Andres Colon <andres@thoughtware.tv>
2015-05-20 16:36:46 -0400

    Script for detecting transactions waiting on SIJC for certificate.

---
+++ systems/prgov/stuck.rb
@@ -0,0 +1,76 @@
+require '../lib/rest'
+require 'resque'
+require 'resque-scheduler'
+require 'resque-retry'
+require 'dotenv'
+
+require 'csv'
+
+# Load our environment variables from the hidden '.env' file in this projects root folder.
+Dotenv.load
+
+require 'app/helpers/config'
+require 'app/helpers/store'
+
+# Setup the configuration
+GMQ::Workers::Config.check
+
+# Resque accepts an existing redis connection, so let's
+# make it use ours.
+# TODO: update this later so that resque gains db reconnection capabilities
+Resque.redis = GMQ::Workers::Store.db
+
+# This is the recommended way to configure Resque but
+# it errors out. So we use what's above.
+# Resque.configure do |configuration|
+#   configuration.redis = GMQ::Workers::Store.db
+# end
+
+# require our jobs & application code.
+# We'll require all existing workers in the core/workers directory
+# These are used both by the Resque workers as well as the Resque-web
+Dir["app/core/workers/*.rb"].each {|file| require file }
+
+puts "\n\nStarting Script\n\n"
+
+values = GMQ::Workers::Store.db.get("gmq:cap:tx:PRCAP1155568347147470178")
+#puts values
+
+keys = [] 
+GMQ::Workers::Store.db.scan_each(:match => "gmq:cap:tx:PRCAP*") {|key| 
+	# append the key but strip the specific redis namespace
+	key = key.to_s.gsub("gmq:cap:tx:", "") 
+	keys << key
+}
+
+puts "Found a total of #{keys.length} transactions in the storage."
+
+waiting = 0
+
+sorted = [] 
+
+keys.each do |key|
+	begin
+		tx = GMQ::Workers::Transaction.find key 
+	rescue Exception => e
+		puts "Error #{e} while processing #{key}. Cannot continue."
+		exit
+	end
+	if(tx.state.to_s == "waiting_for_sijc_to_generate_cert")
+		waiting += 1
+		# puts "#{tx.id} - waiting for SIJC since #{tx.updated_at}" 
+		sorted << { :id => tx.id, :updated_at => tx.updated_at } 
+	end
+end
+
+keys = nil 
+
+
+sorted.sort_by! {|tx| tx[:updated_at] }
+
+sorted.each do |key|
+	puts "#{key[:id]} - waiting for SIJC since #{key[:updated_at]}" 
+end
+
+puts "Found #{waiting} waiting out of #{sorted.length}"  
+puts "Done."
